1003f3620825c4a10c3089452a9ab4acbf7dcb5d
Jason MAGUIRE <jmaguire@e1r3p10.42.fr>
2018-07-22 15:50:24 +0200

    Added library, edited Makefile.

--- lib/libft_creator.sh
+++ lib/libft_creator.sh
@@ -1,35 +1,17 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    Makefile                                           :+:      :+:    :+:    #
+#    libft_creator.sh                                   :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: jmaguire <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2018/07/21 13:53:26 by jmaguire          #+#    #+#              #
-#    Updated: 2018/07/21 13:56:40 by jmaguire         ###   ########.fr        #
+#    Created: 2018/07/15 14:27:28 by jmaguire          #+#    #+#              #
+#    Updated: 2018/07/15 14:31:49 by jmaguire         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-EXEC = program
-SRC = source.c
-CFLAGS = -Wall -Wextra -Werror
-
-# Commands
-
-all:
-	cc $(CFLAGS) -o $(EXEC) $(SRC) -L. -lft
-
-base: $(EXEC)
-
-clean:
-	rm -rf *.o
-
-fclean: clean
-	rm -rf $(EXEC)
-
-re: fclean all
-
-refl: fclean fl
-
-$(EXEC):
-	cc -o $(EXEC) $(SRC)
+#!/bin/bash
+cc -c *.c
+ar rc libft.a *.o
+ranlib libft.a
+rm *.o
